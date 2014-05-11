71585fb354a5da6790733d2c64cfa0ba48b26e8a
Ricardo Kirkner <ricardo@kirkner.com.ar>
2014-05-11 10:36:48 -0300

    rely on gunicorn django integration

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b 0.0.0.0:8000 project.wsgi:application
+web: python manage.py run_gunicorn -b 0.0.0.0:8000
--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gunicorn',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,3 +81,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+
+###########
+# LOGGING #
+###########
+
+LOGGING = {
+    'version': 1
+}
