4fd5b4c765b7650b29c09d1337027875e70891d8
Ricardo Kirkner <ricardo@kirkner.com.ar>
2015-10-29 19:46:45 -0300

    use sqlite as the default database

--- app/project/settings.py
+++ app/project/settings.py
@@ -64,7 +64,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 # Internationalization
